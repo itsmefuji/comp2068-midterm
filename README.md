523d95770377e1b99a3d53c300963060ec2c6426
itsmefuji <yujif.gc@gmail.com>
2016-02-25 13:17:47 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# comp2068-w2016-assignment1
+
+come2068-winter2016-midterm
+Midterm Express Page - Favorite People
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+/*
+Yuji Fujiyama-200300256
+Express Porfolio Assignment
+*/
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
